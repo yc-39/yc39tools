5389ae0e860622306ad1dcf07878098384745bdd
yc39 <963937890@qq.com>
2019-01-23 17:17:45 +0800

    修改new ResultData的初始值

--- src/main/java/com/github/yc_39/entity/ResultData.java
+++ src/main/java/com/github/yc_39/entity/ResultData.java
@@ -10,14 +10,22 @@ public class ResultData {
      * 成功
      */
     public static final String SUCCESS = "0";
+    public static final String MSG_SUCCESS = "成功";
     /**
      * 错误
      */
     public static final String ERROR = "1";
+    public static final String MSG_ERROR = "失败";
     /**
      * 未登陆
      */
     public static final String UNLOGIN = "2";
+    public static final String MSG_UNLOGIN = "未登陆";
+
+    public ResultData() {
+        this.code = SUCCESS;
+        this.msg = MSG_SUCCESS;
+    }
 
     private String code;
     private String msg;
